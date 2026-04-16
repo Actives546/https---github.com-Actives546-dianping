5bb05b4e28ffe0f8764f5cb39fd035175629007f
aliyun7287909785 <mfl0311@aliyun.com>
2026-04-16 10:50:18 +0800

    refactor(用户认证): 移除HttpSession依赖改用Redis存储验证码和token
    重构用户认证相关接口，移除HttpSession参数，改用Redis存储验证码和用户登录token。优化了代码结构，提高了系统的可扩展性和分布式支持能力。

--- src/main/java/com/cclg/dianping/service/IUserService.java
+++ src/main/java/com/cclg/dianping/service/IUserService.java
@@ -5,11 +5,9 @@ import com.cclg.dianping.domain.User;
 import com.cclg.dianping.dto.LoginFormDTO;
 import com.cclg.dianping.dto.Result;
 
-import javax.servlet.http.HttpSession;
-
 public interface IUserService extends IService<User> {
     
-    Result sendCode(String phone, HttpSession session);
+    Result sendCode(String phone);
     
-    Result login(LoginFormDTO loginForm, HttpSession session);
+    Result login(LoginFormDTO loginForm);
 }
